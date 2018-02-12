a89fbe9c4e0f60786d140ad5f48f9ac0cae120f5
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-02-12 07:57:38 +0100

    Fixed numberplats example

--- numbers.py
+++ numbers.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 
-numbers = []
+total = 0
+n = 10
 
-def add_numbers(i, j, k, typ):
-    numbers.append({'i':i, 'j':j, 'k':k, 'typ':typ})
+for i in range(0, n):
+    for j in range(0, n):
+        for k in range(0, n):
+            if (i == 0 and j == 0 and k == 0):
+                continue
 
-for i in range(0, 10):
-    for j in range(0, 10):
-        for k in range(0, 10):
             if i + j == k:
-                add_numbers(i, j, k, "+")
+                total += 1
+                continue
 
             if i - j == k:
-                add_numbers(i, j, k, "-")
+                total += 1
+                continue
 
             if i * j == k:
-                add_numbers(i, j, k, "*")
+                total += 1
+                continue
 
             if j > 0:
                 if i / j == k:
-                    add_numbers(i, j, k, "/")
+                    total += 1
+                    continue
 
 
-print(str(len(numbers)))
-
-print(numbers)
+print(total)
